bcbb5f575b35be66810e2d54344c69c0499e7e0d
egarcia410 <egarcia410@gmail.com>
2017-12-05 11:21:33 -0600

    dokku deploy

--- server/routes.js
+++ server/routes.js
@@ -1,8 +1,19 @@
+const path = require('path');
+const express = require('express');
 const ContactController = require('./controllers/ContactController');
 
 module.exports = (app) => {
+    
     app.post('/create-or-update', ContactController.createOrUpdate);
     app.get('/get-contacts', ContactController.getContacts);
     app.post('/delete-contact', ContactController.delete);
     app.get('/get-contact/:id', ContactController.getContact);
+
+    app.get('/dist/build.js', function (req, res) {
+        res.sendFile(path.normalize(__dirname + '/../client/dist/build.js'));
+    });
+
+    app.all('/*', function (req, res) {
+        res.sendFile(path.normalize(__dirname + '/../client/index.html'));
+    });
 }
