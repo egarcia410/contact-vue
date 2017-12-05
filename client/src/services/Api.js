141587db3844c22c7721e80b9fc9108be4f3bc12
egarcia410 <egarcia410@gmail.com>
2017-12-05 11:27:26 -0600

    dokku deploy

--- client/src/services/Api.js
+++ client/src/services/Api.js
@@ -3,6 +3,6 @@ import axios from 'axios'
 // Creates and instance of axios, setting the base URL to link up with the backend
 export default () => {
     return axios.create({
-        baseURL: `http://localhost:8081/`
+        baseURL: location.href.slice(0, location.href.length - 1)
     })
 }
